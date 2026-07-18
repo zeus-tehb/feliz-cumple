c9728523b63582423aecd2155007ea2ab3855406
zeus-tehb <fga.zeus1@gmail.com>
2026-07-17 22:05:31 -0500

    Add files via upload

---
+++ Sorpresa.js
@@ -0,0 +1,35 @@
+// Carta
+const regalo = document.querySelector(".regalo");
+const regalos = document.querySelector(".regalos");
+const modalCarta = document.getElementById("modalCarta");
+
+regalo.addEventListener("click", () => {
+  modalCarta.classList.add("activo");
+});
+
+regalos.addEventListener("click", () => {
+  modalCarta.classList.add("activo");
+});
+
+modalCarta.addEventListener("click", () => {
+  modalCarta.classList.remove("activo");
+});
+
+// Todo Oscuro + Soplido + Canción
+const overlay = document.querySelector(".overlay");
+const soplido = document.getElementById("soplido");
+const cancion = document.getElementById("cancion");
+const llama = document.querySelector(".llama");
+
+llama.addEventListener("click", () => {
+  soplido.currentTime = 0;
+  soplido.play();
+
+  llama.style.animation = "apagar 0.5s forwards"; // forwards -> Ultimo frame (to)
+
+  setTimeout(() => {
+    cancion.currentTime = 0;
+    cancion.play();
+    overlay.classList.add("hidden");
+  }, 1000);
+});
